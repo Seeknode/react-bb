352059bab84190433046c84fb755cff4548e28b5
liamburgoynenhs <liam.burgoyne@nhs.net>
2017-05-16 21:34:38 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-    entry: './lib/components/Index.js',
+    entry: './lib/renderers/DOM.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
